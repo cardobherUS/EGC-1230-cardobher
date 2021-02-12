224261fee10ecaa626021892efd998b7d2c36ebf
cardobherUS <cardobher@alum.us.es>
2021-02-12 10:27:29 +0100

    arreglando error

--- decide/manage.py
+++ decide/manage.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    #execute_from_command_line(sys.argv)
+    #el cambio ha sido descomentar la linea 16
+    execute_from_command_line(sys.argv)
